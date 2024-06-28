6ddaf2a93851c739d946d60997e371b3015f59e7
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2024-06-28 10:57:36 +0700

    update landing

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -2,8 +2,8 @@ import RecruitmentCountdown from "./shared/recruitment-countdown";
 
 export default function Countdown() {
   return (
-    <div className="bg-orange-700 fixed left-0 right-0 bottom-0 z-50 sm:static">
-      <div className="max-w-screen-xl mx-auto px-4 py-2 sm:py-3 text-white sm:text-center md:px-8 h-14">
+    <div className="bg-yellow-500 fixed left-0 right-0 bottom-0 z-50 sm:static">
+      <div className="max-w-screen-xl mx-auto px-4 py-2 sm:py-3 sm:text-center md:px-8 h-14">
         <RecruitmentCountdown />
       </div>
     </div>
