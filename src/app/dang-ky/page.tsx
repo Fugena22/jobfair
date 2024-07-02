4b89dedce7e69bc8f4cdec4e81a51f3d5aae87ad
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2024-07-02 10:09:02 +0700

    update dang ky page

--- src/app/dang-ky/page.tsx
+++ src/app/dang-ky/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/forms/register-form";
 
 export default function RegisterPage() {
   return (
-    <div className="mx-auto container max-w-md space-y-6 py-12">
+    <div className="mx-auto container max-w-lg space-y-6 py-12">
       <div className="space-y-2 text-center">
         <h1 className="text-3xl font-bold">Đăng ký tham gia ngay</h1>
         <p className="text-muted-foreground">
