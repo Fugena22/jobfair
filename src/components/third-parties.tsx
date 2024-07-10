25cdaeb6d36938a34b15f289b20b3b0c6166525d
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-07-10 14:35:12 +0700

    update

--- src/components/third-parties.tsx
+++ src/components/third-parties.tsx
@@ -23,6 +23,29 @@ export default function ThirdParties() {
         }}
       ></Script>
 
+      {/* <!-- Google Tag Manager --> */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `function(w,d,s,l,i)
+          {w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P7787KZG')`,
+        }}
+      ></Script>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-P7787KZG"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
       {/* <!-- Meta Pixel Code --> */}
       <Script
         id="meta-pixel"
